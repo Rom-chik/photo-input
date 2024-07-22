d9ce20a746232c79213f5523309be9580169cc2f
Rom-chik <anr0manking@gmail.com>
2024-07-23 01:47:47 +0300

    First commit. Design done for icon, username, password form.

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import PhotoInput from './components/PhotoInput.vue'
+
+
+</script>
+
+<template>
+  <div class="flex justify-center items-center w-full h-[100vh] bg-[#F0F0F0]">
+    <PhotoInput />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './index.html',
+        './src/**/*.{vue,js,ts,jsx,tsx}'
+    ],
+    theme: {
+        container: {
+            center: true,
+            screens: {
+                lg: '1800px'
+            }
+        },
+        extend: {
+            colors: {
+                dark: '#6278F7',
+                darker: '#2D2E52',
+                light: '#F2F2F2',
+                lighter: '#F9FAFF',
+                accent: '#95FFD4',
+                'accent-lighter': '#95FFD4'
+            }
+        },
+    },
+    plugins: [],
+}
