24aa789d6008adbfdf340ad02c7e47548d755b50
Rom-chik <anr0manking@gmail.com>
2024-07-31 20:10:35 +0300

    Photo input done. Full scale photo fixed. User data and photo input done on different components.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,28 @@
 <script setup lang="ts">
 import PhotoInput from './components/PhotoInput.vue'
+import UserDataInput from './components/UserDataInput.vue'
 
 
 </script>
 
 <template>
   <div class="flex flex-col justify-center items-center w-full h-[100vh] bg-[#F0F0F0]">
-    <PhotoInput />
+    <div id="card-1" class="flex justify-center items-center w-1/4 min-w-72 h-2/3 bg-[white]">
+        <div class="flex flex-col gap-6 justify-center items-center">
+
+            <h3 class="text-2xl">Fill out the information</h3>
+
+            <div></div>
+
+            <PhotoInput />
+
+            <div></div>
+
+            <UserDataInput />
+
+        </div>
+    </div>
+
   </div>
 </template>
 
---
+++ src/components/UserDataInput.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import {reactive, ref} from "vue";
+
+
+const userData = reactive({
+  username: '',
+  password: ''
+});
+
+const onButtonClick = (async() => {
+  console.log('data submitted', userData)
+});
+
+</script>
+
+<template>
+
+  <!-- username input -->
+  <div class="relative">
+      <input id="username" type="text" v-model="userData.username" class="w-[250px] h-[35px] peer focus:outline-none border-2 border-black focus:border-sky-500 " placeholder=" "/>
+      <label for="username" class="absolute left-1 -top-6 peer-placeholder-shown:top-1 peer-focus:-top-6 transition-all duration-75" >Username</label>
+  </div>
+  <!-- password input -->
+  <div class="relative">
+      <input id="password" type="password" v-model="userData.password" class="w-[250px] h-[35px] peer focus:outline-none border-2 border-black focus:border-sky-500 " placeholder=" "/>
+      <label for="password" class="absolute left-1 -top-6 peer-placeholder-shown:top-1 peer-focus:-top-6 transition-all duration-75" >Password</label>
+  </div>
+  <!-- submit button -->
+  <div>
+      <button @click="onButtonClick()" type="submit" class="border border-black hover:bg-[#F0F0F0] w-[75px] h-[35px]">Submit</button>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
